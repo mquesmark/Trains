5352cf455316f52512265f0874d47ffb8c624f4c
Maxim <mquesmark@icloud.com>
2025-11-25 04:54:05 +0300

    Initial Commit

---
+++ Trains/TrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsApp.swift
+//  Trains
+//
+//  Created by Максим on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
